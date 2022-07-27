af9969f596f137f5b4a425a9b95b5e8853d45760
malaksa23-meet <109904323+malaksa23-meet@users.noreply.github.com>
2022-07-27 12:48:04 +0300

    Finished

--- Advanced-Flask-Lab/app.py
+++ Advanced-Flask-Lab/app.py
@@ -9,15 +9,15 @@ app = Flask(  # Create a flask app
 )
 
 # Variables for tasks
-image_link = "https://scontent.ftlv21-1.fna.fbcdn.net/v/t31.18172-1/1502644_10152506946962507_2442851309927378964_o.png?stp=c49.0.148.148a_dst-png_p148x148&_nc_cat=111&ccb=1-7&_nc_sid=1eb0c7&_nc_ohc=d-R_wjIm2d8AX_NugPv&_nc_ht=scontent.ftlv21-1.fna&oh=00_AT8CSjiG8-biys_qQ9tU-yrxzJSp6HANxsz5c3mFVk-G2Q&oe=62FD0A1B"
+image_link = "https://www.youthdynamics.org/wp-content/uploads/2017/11/teen-engage.png"
 
-user_bio = "Middle East Entrepreneurs of Tomorrow. Enabling the next generation of Israeli and Palestinian leaders."
+user_bio = "Hello Hello , it's Lour , 19YO, I'm a photographer:) "
 
 posts = {
-    "https://scontent.ftlv1-1.fna.fbcdn.net/v/t39.30808-6/243021542_10158462822407507_4186737446569136175_n.jpg?stp=cp0_dst-jpg_e15_fr_q65&_nc_cat=101&ccb=1-7&_nc_sid=ed5ff1&_nc_ohc=nHa7N_8RkP0AX92LvUb&_nc_ht=scontent.ftlv1-1.fna&oh=00_AT8kLSHR6V-bvFjpXVVzHXWXviYhBJgkgHANsGlmTojibg&oe=62E08013": "2021 cohort's Y3 Accelerator!",
-    "https://media-exp1.licdn.com/dms/image/C4D1BAQFTbpgMk3KTSg/company-background_10000/0/1614595305396?e=1659178800&v=beta&t=OiSIvxsPJiJkArJIzBCKVF0_-yEta9gv1qLVnViU8bo": "MEET graduation!",
-    "https://pbs.twimg.com/media/FPvsO6xVkAEcrBm?format=jpg&name=900x900": "#Throwback to one of our favorite #MEETsummer events: #BowlingNight!",
-    "https://pbs.twimg.com/media/FI_UkcnVIAAUvWN?format=jpg&name=medium": "2020 cohort in their Y1 summer!"}
+   
+    "https://media-cdn.tripadvisor.com/media/vr-splice-j/06/cf/76/54.jpg": "<33333",
+    "https://www.allaboutbirds.org/news/wp-content/uploads/2020/07/STanager-Shapiro-ML.jpg": "woooooow ",
+    "https://upload.wikimedia.org/wikipedia/commons/5/58/Sunset_2007-1.jpg": "Such an amzing view <33"}
 
 
 #####
@@ -25,7 +25,7 @@ posts = {
 
 @app.route('/')  # '/' for the default page
 def home():
-    return render_template('index.html')
+    return render_template('index.html',image_link=image_link,user_bio=user_bio,posts=posts)
 
 
 @app.route('/about')  # '/' for the default page
